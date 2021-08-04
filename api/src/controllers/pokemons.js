f88c1275876670e539451de8ef4cf99ac738c9c2
Hermes Aguilar <adanhermes23@gmail.com>
2021-08-04 16:21:04 -0500

    CRUD casi terminado

--- api/src/controllers/pokemons.js
+++ api/src/controllers/pokemons.js
@@ -1,7 +1,8 @@
 const {Pokemon, Type } = require('../db');
-const { V4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const {API_POKEMONS_ENDPOINT} = require('../constants');
 const axios = require('axios');
+
 
 async function getPokemons(req, res, next) {
 
@@ -11,15 +12,14 @@ async function getPokemons(req, res, next) {
 
     try {
         let myPokemon = await Pokemon.findAll({ include: Type });
-        console.log(myPokemon);
         myPokemon = myPokemon.map((element) => {
             return {
                 id: element.dataValues.id,
                 image: element.dataValues.image,
                 name: element.dataValues.name,
                 attack: element.dataValues.attack,
-                type1: element.dataValues.types[0].name,
-                type2: element.dataValues.types[1].name,
+                type1: element.dataValues.types[0]?.name,
+                type2: element.dataValues.types[1]?.name,
             };
         });
 
@@ -55,6 +55,120 @@ async function getPokemons(req, res, next) {
 
 }
 
+
+async function getPokemonById(req, res, next){
+    const id = req.params.idPokemon;
+
+    try {
+        
+        pokemonApi = await axios.get(`${API_POKEMONS_ENDPOINT}${id}/`)
+        pokemonApi = pokemonApi.data;
+        console.log(pokemonApi)
+        
+        let showPokemon = {
+            image: pokemonApi.sprites.other.dream_world.front_default,
+            name: pokemonApi.name,
+            type1: pokemonApi.types[0].type.name,
+            type2: pokemonApi.types[1].type.name,
+            id: pokemonApi.id,
+            height: pokemonApi.height,
+            weight: pokemonApi.weight,
+            life: pokemonApi.stats[0].base_stat,
+            attack: pokemonApi.stats[1].base_stat,
+            defense:pokemonApi.stats[2].base_stat,
+            speed: pokemonApi.stats[5].base_stat,
+        };
+        console.log(showPokemon)
+
+        res.send(showPokemon);
+
+    }catch(error){
+        Pokemon.findByPk(id, { include: Type} )
+        .then((pokemon) => {
+            res.send(pokemon);
+        })
+        .catch((error) => {
+            res.status(500).send("Not Found!");
+        });
+    }
+}
+
+async function getPokemonByName(req, res, next) {
+
+    const name = req.query.name;
+
+    try{
+        let pokemonApi = await axios.get(`${API_POKEMONS_ENDPOINT}${name}`)
+        pokemonApi = pokemonApi.data
+
+        let showPokemon = {
+            image: pokemonApi.sprites.other.dream_world.front_default,
+            name: pokemonApi.name,
+            type1: pokemonApi.types[0].type.name,
+            type2: pokemonApi.types[1]?.type.name,
+            id: pokemonApi.id,
+            height: pokemonApi.height,
+            weight: pokemonApi.weight,
+            life: pokemonApi.stats[0].base_stat,
+            attack: pokemonApi.stats[1].base_stat,
+            defense:pokemonApi.stats[2].base_stat,
+            speed: pokemonApi.stats[5].base_stat,
+        };
+        res.send(showPokemon);
+
+    }catch(error){
+        Pokemon.findOne({ where: {name: name}, include: Type })
+        .then((pokemon) => {
+            if (pokemon) {
+                res.send(pokemon);
+            } else {
+                res.status(500).send("Not found!");
+            }
+        })
+        .catch((error) => {
+            next(error);
+        })
+    }
+
+}
+
+async function createPokemon (req, res, next) {
+    const {
+        name,
+        life,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+        type1,
+        type2,
+        image
+    } = req.body
+
+    let newPokemon = {
+        id: uuidv4(),
+        name: name,
+        life: life,
+        attack: attack,
+        defense: defense,
+        speed: speed,
+        height: height,
+        weight: weight,
+        image: image,
+    }
+
+    let types = [type1, type2 ? type2 : null];
+    return Pokemon.create(newPokemon)
+    .then((pokemon) => {
+        pokemon.addTypes(types);
+        res.send({...pokemon, types })
+    })
+    .catch((error) => next(error));
+}
 module.exports={
-    getPokemons
+    getPokemons,
+    getPokemonById,
+    getPokemonByName,
+    createPokemon
 }
--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,7 +1,12 @@
 const { Pokemon, Type } = require('../db');
 const { API_POKEMONS_ENDPOINT} = require('../constants')
 const { default: axios } = require("axios");
-const {getPokemons} = require('../controllers/pokemons.js')
+const {getPokemons,
+       getPokemonById,
+       getPokemonByName,
+       createPokemon
+
+} = require('../controllers/pokemons.js')
 const {Router} = require('express')
 
 
@@ -10,13 +15,11 @@ const router = Router()
 
 router.get('/',getPokemons);
 
-router.get('/:id',(req,res) => {
+router.get('/:idPokemon', getPokemonById);
 
-})
+router.get('', getPokemonByName);
 
-router.post('/', (req, res) => {
-
-})
+router.post('/', createPokemon);
 
 
 module.exports = router
